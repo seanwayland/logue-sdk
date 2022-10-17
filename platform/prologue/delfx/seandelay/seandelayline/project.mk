dfe79c164ef88563feb270a4502d50c81e0e6128
seanwayland <seanwayland@gmail.com>
2022-10-16 20:41:57 -0400

    hello world copy of delay

---
+++ platform/prologue/delfx/seandelay/seandelayline/project.mk
@@ -0,0 +1,17 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+PROJECT = seandelayline
+
+UCSRC = 
+
+UCXXSRC = ../src/seandelayline.cpp
+
+UINCDIR =
+
+UDEFS =
+
+ULIB = 
+
+ULIBDIR =
